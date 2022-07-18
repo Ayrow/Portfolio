a45cf059624a0290ddcd2641bc86ca0277903347
Ayrow <aymeric.pilaert@hotmail.fr>
2022-07-18 11:59:44 +0200

    added a Navbar and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
+import { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Fragment>
+  );
+};
 
 export default App;
