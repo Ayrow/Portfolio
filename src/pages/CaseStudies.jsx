7506adcb112dd8fff7ff0cd8c258b179a3274089
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-06 18:08:43 +0200

    basic design and effects for the projects cards

--- src/pages/CaseStudies.jsx
+++ src/pages/CaseStudies.jsx
@@ -1,31 +1,34 @@
 import { projectLinks as links } from '../utils/links';
 import { Link } from 'react-router-dom';
-import Wrapper from '../assets/wrappers/caseStudies';
 
-const CaseStudies = ({ imgObject }) => {
+const CaseStudies = () => {
   return (
     <div className='mt-10'>
       <h2 className=' text-5xl text-center'>Case Studies</h2>
 
       <h3 className='text-center text-xl'>Latest projects</h3>
-      <div className='flex justify-center mt-10'>
+      <div className='flex flex-col place-items-center '>
         {links.map((link) => {
           const { id, title, text, imageLink, path } = link;
           return (
-            <Link to={path} key={id}>
-              {/* 
-              <img
-                src={imageLink}
-                alt={title}
-                width='50%'
-                height='50%'
-                className=''
-              />
-              */}
-
-              <h3> {title} </h3>
-              <p> {text} </p>
-              <button>Check it out</button>
+            <Link
+              to={path}
+              key={id}
+              className='my-5 shadow-xl shadow-slate-800 '
+              style={{
+                backgroundImage: `url('${imageLink}')`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                width: '80%',
+                height: '480px',
+              }}>
+              <div className=' bg-opacity-50 bg-black lg:hover:bg-gray-800 lg:hover:bg-opacity-95 w-full h-full transition-all ease-in-out duration-500 text-white'>
+                <div className='w-full h-full flex flex-col place-items-center place-content-center lg:hover:scale-110 ease-in-out lg:hover:ease-in-out transition-transform duration-500'>
+                  <h3 className=' text-5xl'> {title} </h3>
+                  <p className='py-5'> {text} </p>
+                  <button className='btn'>Check it out</button>
+                </div>
+              </div>
             </Link>
           );
         })}
--- src/utils/links.js
+++ src/utils/links.js
@@ -21,7 +21,16 @@ const projectLinks = [
     id: 1,
     title: 'Ceresa Redesign',
     text: 'Leader in bathroom furniture since 1983',
-    imageLink: require('../assets/images/test-image.png'),
+    imageLink:
+      'https://www.depreux-construction.com/wp-content/uploads/2021/02/salon-constructeur-maison-individuelle.jpg',
+    path: '/',
+  },
+  {
+    id: 2,
+    title: 'Neossa',
+    text: 'Personal freelance website made using Wordpress',
+    imageLink:
+      'https://www.depreux-construction.com/wp-content/uploads/2021/02/salon-constructeur-maison-individuelle.jpg',
     path: '/',
   },
 ];
