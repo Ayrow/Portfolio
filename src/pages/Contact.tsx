fa8583c6afc541d6e68095765c409a953cf9a54a
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-13 13:38:13 +0100

    added button in contact section to send message

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,18 @@
 const Contact = () => {
   return (
-    <div className='mt-10' id='contact'>
-      <h2 className=' text-5xl text-center'>Contact</h2>
+    <div className='mt-10 flex flex-col items-center' id='contact'>
+      <div className='w-1/3 flex flex-col items-center gap-10'>
+        <h2 className=' text-5xl text-center'>Contact</h2>
+        <p>
+          Sorry I did not want to connect to a backend or another service but
+          you can click the button down below to contact me
+        </p>
+        <a
+          href='mailto:aymeric.pilaert@hotmail.fr'
+          className='text-center w-1/2 px-5 py-2 rounded-md bg-gray-800 hover:bg-gray-700 text-white'>
+          Send a message
+        </a>
+      </div>
     </div>
   );
 };
